611c8ccd7bf7331b3d37626b61edb783d6c0a940
Joseph Dey <josephdey@uchicago.edu>
2024-08-24 13:49:58 -0400

    setup and used eslint and prettier

---
+++ eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    extends: ['airbnb', 'plugin:prettier/recommended'],
+    plugins: ['react', 'react-hooks', 'jsx-a11y'],
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+      'react/react-in-jsx-scope': 'off',
+    },
+  };
